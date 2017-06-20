296a17514ea6e5c0d6d2b07395893076c331c18e
Michele Cipriano <micco00x@gmail.com>
2017-06-20 17:13:49 +0200

    Sun added

--- src/Main.js
+++ src/Main.js
@@ -32,11 +32,49 @@ window.onload = function() {
 	// Texture loader:
 	var textureLoader = new THREE.TextureLoader();
 	
+	// Sun parameters:
+	var sunRadius = 100; // sun: 695700km
+	var sunRotationSpeed = 0.01;
+	var sunRevolutionSpeed = 0;
+	var sunOrbitalDistance = 0;
+	var sunChunkPerFaceSide = 8;
+	var sunLodParams = [[25, 10], [20, 50], [15, 100], [5, 200]];
+	var sunUniforms = THREE.UniformsUtils.merge([THREE.UniformsLib["lights"],
+												  {
+												  planetPosition: { type: "v3", value: new THREE.Vector3(0, 0, 0) },
+												  radius: { value: sunRadius },
+												  texture: { value: Array(8).fill(null) },
+												  textureHeight: { type: "fv1", value: [0.8, 1.0, 1.15, 1.35, 1.6, 1.75, 2.0, 2.5] }
+												  }]);
+	
+	// Texture loader lods images asynchronously:
+	textureLoader.load("images/sun/sun0.png", function(texture) {
+					   sunUniforms.texture.value[0] = texture;
+					   sunUniforms.texture.value[1] = texture;
+					   sunUniforms.texture.value[2] = texture;
+					   sunUniforms.texture.value[3] = texture;
+					   sunUniforms.texture.value[4] = texture;
+					   sunUniforms.texture.value[5] = texture;
+					   sunUniforms.texture.value[6] = texture;
+					   sunUniforms.texture.value[7] = texture;
+					   });
+	
+	var sunMaterial = new THREE.ShaderMaterial({ uniforms: sunUniforms,
+												//attributes: attributes,
+												vertexShader: document.getElementById("basicVertexShader").textContent,
+												fragmentShader: document.getElementById("basicFragmentShader").textContent,
+												lights: true
+												});
+	
+	var sun = new Planet("sun", sunRadius, sunRotationSpeed, sunRevolutionSpeed, sunOrbitalDistance,
+						  sunChunkPerFaceSide, sunLodParams, sunMaterial, noiseHeightGenerator);
+	scene.add(sun);
+	
 	// Earth parameters:
 	var earthRadius = 40; // earth: 6371km
 	var earthRotationSpeed = 0.02;
-	var earthRevolutionSpeed = 0;
-	var earthOrbitalDistance = 0;
+	var earthRevolutionSpeed = 0.05;
+	var earthOrbitalDistance = 200;
 	var earthChunkPerFaceSide = 8;
 	var earthLodParams = [[25, 10], [20, 50], [15, 100], [5, 200]];
 	var earthUniforms = THREE.UniformsUtils.merge([THREE.UniformsLib["lights"],
@@ -99,7 +137,7 @@ window.onload = function() {
 
 	var earth = new Planet("earth", earthRadius, earthRotationSpeed, earthRevolutionSpeed, earthOrbitalDistance,
 						   earthChunkPerFaceSide, earthLodParams, earthMaterial, noiseHeightGenerator );
-	scene.add(earth);
+	sun.add(earth);
 	
 	// Moon parameters:
 	noiseHeightGenerator.scale = 35;
@@ -142,10 +180,10 @@ window.onload = function() {
 
 	// TODO: Manage light in the shader
 	var light = new THREE.PointLight();
-	light.position.set( 0, 0, 80 );
+	light.position.set( 0, 0, 150 );
 	scene.add( light );
 
-	camera.position.z = 120;
+	camera.position.z = 300;
 
 	var clock = new THREE.Clock();
 	var time = 0;
@@ -155,7 +193,6 @@ window.onload = function() {
 	controls.domElement = document.body;
 	
 	var render = function () {
-		
 		delta = clock.getDelta();
 		time += delta;
 		
@@ -164,10 +201,12 @@ window.onload = function() {
 		requestAnimationFrame(render);
 		
 		// Update position of the planets:
+		sun.updatePosition(time);
 		earth.updatePosition(time);
 		moon.updatePosition(time);
 		
 		// Send position of the planets to the shader:
+		sunUniforms.planetPosition.value = sun.position;
 		earthUniforms.planetPosition.value = earth.position;
 		moonUniforms.planetPosition.value = moon.position;
 		
